0996ed76d77ad9f781d902d7621fc52d612c8c0f
Jimmy Yeung <worldofpuzzle@yahoo.com>
2021-11-18 15:03:37 -0800

    I need help on card to be put on the same row.

--- foodpedia/src/components/About.js
+++ foodpedia/src/components/About.js
@@ -1,10 +1,43 @@
 import React from 'react'
-
+import { Card } from 'react-bootstrap'
 const About = () => {
     return (
-        <div>
+        <div class="container center-div">
+            
+        <div class="row-md-15">
+            <div class="col-md-4">
             About
+            <br></br>
+            We are a "Mission statement type thing"
+            <br></br>
+            Meet the team: Sriniti
+                            <br></br>
+                            Chris
+                           <br></br>
+                           Jimmy
+                           <br></br>
+                           Dev
+                           <br></br>
+
         </div>
+        <div class="col-md-10">
+        <Card style={{ width: '18rem' }}>
+  <Card.Body>
+    <Card.Title>General Information</Card.Title>
+    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+</Card>
+</div>
+        </div>
+
+        </div>
+        
     )
 }
 

2e4c1b7a585a4a74f861f8acf5521a7d94146728
chrisquinn135 <85804252+chrisquinn135@users.noreply.github.com>
2021-12-07 11:27:26 -0800

    mostly done with styling

--- foodpedia/src/components/Homepage.js
+++ foodpedia/src/components/Homepage.js
@@ -5,27 +5,11 @@ import { CardGroup } from 'react-bootstrap'
 import card1 from '../images/card1.jpg'
 import card2 from '../images/card2.jpg'
 import card3 from '../images/card3.jpg'
+import './Homepage.css'
 import { useNavigate } from 'react-router'
 
 
 const Homepage = () => {
-
-    const homepageStyle = {
-        display: 'grid',
-        gridTemplateColumns: '1fr 1fr'
-    }
-
-    const missionStyle = {
-        fontStyle: 'normal',
-        fontWeight: 500,
-        fontSize: '36px',
-        lineHeight: '36px',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        textAlign: 'center',
-        lineHeight: '60px'
-    }
 
     const navigate = useNavigate()
     const onClick = () => {
@@ -35,21 +19,21 @@ const Homepage = () => {
     return (
 
         <div>
-            <div style={homepageStyle}>
-                <div style={missionStyle}>
+            <div className='homeInfo'>
+                <div className='mission'>
                     A discounted food delivery service<br/> 
                     focusing on freshly prepared <br />unsold ingredients to prevent <br />restaurant
                     food waste, making it both
                     <br />affordable AND sustainable
 
                 </div>
-                <div>
-                    <img src={food} alt="Logo" style={{ width: "75%", display: "block" }} />
-                </div>
+
+                    <img src={food} alt="Logo" />
+
             </div>
-            <div style={missionStyle} >
-                <CardGroup style={{ gap: 250 }}>
-                    <InfoCard img={card1} title='Top Seller' name='Johnnies and Co' />
+            <div className='mission' >
+                <CardGroup style={{justifyContent:'space-evenly' }}>
+                    <InfoCard className='test' img={card1} title='Top Seller' name='Johnnies and Co' />
                     <InfoCard img={card2} title='Most Liked' name='Lady W' />
                     <InfoCard img={card3} title='Deal of the Day' name='Banda Express' />
                 </CardGroup>

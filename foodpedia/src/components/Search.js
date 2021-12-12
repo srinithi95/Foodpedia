5f39347838421db3f8f6ad5c037ee45cf76e3e53
srinithi95 <60716676+srinithi95@users.noreply.github.com>
2021-12-12 11:36:20 -0800

    pagination for search page

--- foodpedia/src/components/Search.js
+++ foodpedia/src/components/Search.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import {connect} from 'react-redux'
 import search from '../images/search.png'
 import ToolMap from './ToolMap'
@@ -6,8 +6,12 @@ import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
 import "./Search.css";
 import { CardGroup } from 'react-bootstrap'
 import InfoCard from './InfoCard'
+import Pagination from './Pagination';
+import Card from "./card/Card.jsx";
 
 const Search = (props) => {
+    const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(9);
   const coordinates=[{
     lat:37.733795,
     lng:-122.446747
@@ -51,36 +55,77 @@ const Search = (props) => {
     {
         name:"Popeyes",
         image:search
-    }, {
+    }, 
+    {
+        name:"WcDonalds",
+        image:search
+    },
+    {
+        name:"WcDonalds",
+        image:search
+    },
+    {
+        name:"WcDonalds",
+        image:search
+    },
+    {
+        name:"WcDonalds",
+        image:search
+    },
+    {
+        name:"WcDonalds",
+        image:search
+    },
+    {
         name:"WcDonalds",
         image:search
     }
     ]
-
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const current_search_Results = search_results.slice(indexOfFirstPost, indexOfLastPost);
+    const paginate = pageNumber => setCurrentPage(pageNumber);
     return (
         <div className="container-fluid">
-      <div className="col-6">
-      <div id="col-3 heading col2">
-          <b>Showing 1-20 of 30</b>
+      <div className="row" id="heading">
+          <div style={{paddingLeft:"5%"}} className="col col-lg-5">
+          <p  style={{fontSize:"20px", paddingRight:"50%"}}>Showing 1-9 of 20</p>
           </div>
-      </div>
-      <br/>
-      <div  id="map-container">
+     <div className="col col-md-3">
+          <select id="inputState" class="form-control"  required>
+            <option>Sort By</option>
+            <option value="AS">Rating</option>
+            <option value="GU">Most Popular</option>
+            <option value="MP">Delivery time</option>
+           </select>
+       </div>
+       </div>
+       <br/>
+       <div id="map-container">
     <ToolMap coordinates={coordinates}/><br/>
    </div>
-    <div id="card-display" className="row custom-card">
-    <CardGroup style={{ gap: 150 }} className="col-md-3 mb-2">
-    {search_results.map((product) => (
+   <div id="card-display" style={{paddingLeft:"5%", minWidth:"18rem"}} className="col-10  mb-4 custom-card">
+    <CardGroup style={{ gap: 80, marginBottom: "10px" }} >
+    {current_search_Results.map((product) => (
+        <div className="col-md-3 mb-2">
           <InfoCard img={product.image} title={product.name} name='Johnnies and Co'/>
+         
+          </div>
         ))}
      </CardGroup>
+     
     
     </div>
+    <div style={{paddingLeft:"40%"}}>
+    
+    < Pagination
+        postsPerPage={9}
+        totalPosts={11}
+        paginate={paginate}
+      />
+    </div>
+  
         </div> 
-       
-        
-       
-       
     )
 }
 

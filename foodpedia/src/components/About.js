1f0458d86bb4adc85d830dfeeab51287c6e304c2
Jimmy Yeung <worldofpuzzle@yahoo.com>
2021-12-13 15:15:25 -0800

    I adjust the about page to center and fixed the font size.

--- foodpedia/src/components/About.js
+++ foodpedia/src/components/About.js
@@ -12,42 +12,45 @@ const About = () => {
   
 
  
-  <div class="col-sm-6" id="center">
-    <div class="card">
-      <div class="card-body">
+  <div class="col-sm-15" id="center">
+    <div class="card" >
+      <div class="card-body" >
       <h2>Mission Statement</h2>
-        <h5 class="left-text"> </h5>
+        <h5 class="left-text"  > </h5>
         
-        <div class="general">
+        <div class="general"  >
+          <h4>
         All of our members are senior students of San Francisco State univeristy.
         A food ordering and delivery application to save food, make it affordable to customers and profitable to restaurants.
         FoodPedia is a food ordering and delivery application which would help people to order food at a discounted price using 
         an innovative idea. Our application focuses on saving the food and helping people utilize it, before it is wasted, at a much lower price.
          We partner with restaurants and they can list their leftover food menu items or ingredients at a lower rate than the competitors, without 
          any additional fees, to save their food from going to waste.
-         
+         </h4>
          <div class="right-picture" >
-         <img src ={smartphone} style={{ width: "100%"}}></img>
+         <img src ={smartphone} style={{ width: "50%"}}></img>
          </div>
-        
+         <h4>
         <br></br>
         Our focus is to save the leftover food and ingredients in restaurants at the end of each day which would either go to waste or expire. For restaurants, it is
         extremely difficult to order the precise amount of food needed to satisfy demand on a weekly/ biweekly basis. Those restaurants can sell the leftover food or freshly prepared food with leftover ingredients at a discounted price using our application. The user can search and order food from all the nearby restaurants and specifically those that are partnered with us and provide food at discounted prices. The user can make use of various filters like cuisines, location and discounts. To deliver your food faster and in an affordable manner, we have partnered with local delivery services and companies. We use their portal to forward the delivery instructions and the delivery is fulfilled by the nearest driver in their database. Delivery costs are minimal depending on your location but will be waived if you are our Pro Member.The motivation behind this application is to make the food more affordable to people and also make the restaurants more profitable by saving the food, ingredients and the environment. All surplus meals bought through FoodPedia helps restaurants cut down on costs, gain exposure and compete with big restaurant chains. To encourage the practice of saving the environment and food, we will reimburse all partnering restaurants for the packaging material used for the orders if they are 100% compostable and eco-friendly. Just send us a receipt and we got you covered.
-
+        </h4>
         </div>
         <br></br>
         <p class="card-text"></p>
         <h2>General Information</h2>
         <div class="general">
+        <h4>
         We are saving people from hunger, poverty, and time.  Our focus is to save the leftover food and ingredients in restaurants
          at the end of each day which would either go to waste or expire. To deliver your food faster and in an affordable manner, 
          we have partnered with local delivery services and companies. The motivation behind this application is to make the food more
           affordable to people and also make the restaurants more profitable by saving the food, ingredients and the environment. 
           To encourage the practice of saving the environment and food, we will reimburse all partnering restaurants for the packaging 
           material used for the orders if they are 100% compostable and eco-friendly. 
+        </h4>
           </div>
           <div class="left-picture" >
-         <img src ={ecofriendly} style={{ width: "100%"}}></img>
+         <img src ={ecofriendly} style={{ width: "50%"}}></img>
          </div>
         <br></br>
         <br></br>

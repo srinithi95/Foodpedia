d4fb77b8f539cae89d3dc544b09828497b44146c
Jimmy Yeung <worldofpuzzle@yahoo.com>
2021-12-13 11:14:21 -0800

    I added pagination.

--- foodpedia/src/components/Restaurant.jsx
+++ foodpedia/src/components/Restaurant.jsx
@@ -5,34 +5,51 @@ import Card from "./card/Card.jsx";
 import Filter from "./filter/Filter.jsx";
 import '../components/stylesheet.css';
 import star from '../components/star.jpg';
+import InfoCard from './InfoCard'
+import Pagination from './Pagination';
+ 
 // reference was https://codesandbox.io/s/react-ecommerce-forked-wp1x1?file=/src/components/core/home/Home.jsx and access date was 11/22/21
 // Restaurant should have certain names to be displayed.
 const Restaurant = () => {
+
   const [products, setProducts] = useState([]);
-  const [filteredProducts, setFilteredProducts] = useState([]);
+  let [filteredProducts, setFilteredProducts] = useState([]);
   const [sortBy, setSortBy] = useState("");
   const [selectedTag, setSelectedTag] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(9);
 
   // restaurant has food
   useEffect(() => {
-    setProducts(data);
-    setFilteredProducts(data);
-  }, []);
+    filteredProducts = data.slice(indexOfFirstPost, indexOfLastPost);
+    
+    setFilteredProducts(filteredProducts);
+    
+  }, [ filteredProducts, setFilteredProducts]);
 
   useEffect(() => {
+    
     const filtered = selectedTag
-      ? products.filter((item) => item.tag === selectedTag)
-      : products;
+      ? filteredProducts.filter((item) => item.tag === selectedTag)
+      : filteredProducts;
 
     setFilteredProducts(
+      
       sortBy
         ? [...filtered].sort((a, b) =>
             sortBy === "lowest" ? a.price - b.price : b.price - a.price
           )
         : [...filtered].sort((a, b) => (a.id > b.id ? 1 : -1))
     );
-  }, [selectedTag, sortBy, products]);
+  }, [selectedTag, sortBy, filteredProducts]);
 
+  
+
+
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  
   return (
     <div className="container-fluid">
        
@@ -40,11 +57,14 @@ const Restaurant = () => {
     <div class="div2"><h3>30-40 Min : 4.4  </h3> </div> 
     <br></br>
     <br></br>
-      <Filter
+      <Filter 
         handleSort={setSortBy}
         handleTagChange={setSelectedTag}
         selectedTag={selectedTag}
         sortBy={sortBy}
+        filteredProducts={9}
+        postsPerPage={9}
+        totalPosts={11}
       />
      
       <div className="row custom-card" style={{marginLeft:50,marginRight:50}}>
@@ -54,6 +74,16 @@ const Restaurant = () => {
           </div>
         ))}
       </div>
+ 
+      <div style={{paddingLeft:"40%"}}>
+    
+      <Pagination
+        filteredProducts={9}
+        postsPerPage={9}
+        totalPosts={10}
+        paginate={paginate}
+      />
+    </div>
     </div>
   )
 }
--- foodpedia/src/components/filter/Filter.jsx
+++ foodpedia/src/components/filter/Filter.jsx
@@ -2,19 +2,37 @@ import React from "react";
 import "./Filter.css";
 
 function Filter({ handleSort, handleTagChange, selectedTag, sortBy }) {
-  let tagNames = ["All", "Noodle", "Rich Dishes", "Egg Rolls"];
-
+  let tagNames = ["Noodle", "Rich Dishes", "Egg Rolls"];
+ 
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-12 col-md-9">
-          <p className=" mr-2" style={{textAlign:'left',marginLeft:'5rem'}}>
-                      
+          
+          <p className=" mr-2" style={{textAlign:'left',marginLeft:'7rem'}}>
+
+          <p> </p>{["All"].map((all, idx) => (
+              <button
+              style={{marginRight:10,width:'7rem', border: 'none'}}
+                key={idx}
+                type="button"
+                className={`btn mr-2 custom-filter ${
+                  selectedTag === all
+                    ? "btn-secondary"
+                    : "btn-outline-secondary"
+                }`}
+                onClick={(e) => handleTagChange(e.target.value)}
+
+              >
+                {all}
+              </button>
+            ))} 
+
             { /* This displays certain items for certain buttons. */
             tagNames.map((tag, idx) => (
               <button
                 key={idx}
-                style={{margin:10,width:'7rem'}}
+                style={{margin:10,width:'7rem', border: 'none'}}
                 type="button"
                 className={`btn mr-2 custom-filter ${
                   selectedTag === tag
@@ -27,6 +45,10 @@ function Filter({ handleSort, handleTagChange, selectedTag, sortBy }) {
                 {tag}
               </button>
             ))}
+ 
+
+
+ 
           </p>
         </div>
       </div>
